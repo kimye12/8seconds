e924b983b53feca02776096a2d79d6929ff97579
kimye12 <lwsd0236wh@naver.com>
2025-09-30 14:41:50 +0900

    Update CategoryWrapper.jsx

--- src/components/Pages/CategoryWrapper.jsx
+++ src/components/Pages/CategoryWrapper.jsx
@@ -4,6 +4,6 @@ import { useLocation } from "react-router-dom";
 export default function CategoryWrapper() {
   const location = useLocation();
   const code = location.pathname.slice(1); // '/' 제거
-  console.log(code);
+
   return <CategoryPage key={code} code={code} />;
 }
